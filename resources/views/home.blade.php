0184a7977ad9e5924f75aa372755f161879b8e73
liviaAugusta <liviamattoni@gmail.com>
2022-09-21 17:14:52 +0200

    creazione di un model con relativa migration e un resource controller.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.main')
+
+@section('title', 'home')
+    
+@section('content')
+    <div class="card-title text-center">
+        <img src="{{ asset('images/logo.png') }}" alt="Logo-DC">
+        <h1>Comics</h1>
+    </div>
+    @endsection
